9e2ecb8596fcf1cdb839c5c726798ad5c33e0f1a
tcook55 <43211149+tcook55@users.noreply.github.com>
2018-10-15 12:12:19 -0400

    Add files via upload

---
+++ csvcleaner.py
@@ -0,0 +1,33 @@
+import csv
+
+class Point:
+    def __init__(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+        
+user_dict = {}
+old_ctr = 0
+new_ctr = 0
+seen = set()
+with open('/home/tylercook/Jenkins_Project/Users/users.csv', mode='r') as unclean_f:
+    parser = csv.reader(unclean_f, delimiter = ',')
+    with open('/home/tylercook/Jenkins_Project/Users/users_clean.csv', mode='w', newline = '') as clean_f:
+        writeCSV = csv.writer(clean_f, delimiter = ',')
+        for row in parser:
+            old_ctr += 1
+            point_check = Point(row[3], row[2])
+            if (point_check.x, point_check.y) not in seen:
+                seen.add((point_check.x, point_check.y))
+                writeCSV.writerow([row[0], row[1], row[2], row[3]])
+                if row[0] in user_dict:
+                    user_dict[row[0]] += 1
+                else:
+                    user_dict[row[0]] = 1
+                new_ctr += 1
+
+
+print('Old number of data points: ' + str(old_ctr))
+print('New number of data points: ' + str(new_ctr))
+print('================================')
+for key in user_dict:
+    print("{}: {}".format(key, user_dict[key]))
